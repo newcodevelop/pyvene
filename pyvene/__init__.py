3888ec990876bf3d862300a9ea8fcbb28da98c8a
Dibyanayan Bandyopadhyay <dibyanayan@gmail.com>
2024-12-12 16:04:30 +0530

    Update __init__.py

--- pyvene/__init__.py
+++ pyvene/__init__.py
@@ -52,4 +52,5 @@ from .models.gru.modelings_gru import GRUConfig
 from .models.llama.modelings_intervenable_llama import create_llama
 from .models.mlp.modelings_intervenable_mlp import create_mlp_classifier
 from .models.backpack_gpt2.modelings_intervenable_backpack_gpt2 import create_backpack_gpt2
+from .models.mistral.modellings_intervenable_mistral import create_mistral
 
